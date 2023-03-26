9d87ce4ae912bba7be93e0b73350253ddf436bf0
amir62hossein <amirnourian378@gmail.com>
2023-03-26 16:18:26 +0430

    init

---
+++ app/startup/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const winston = require('winston');
+const config = require('config');
+
+module.exports = function() {
+    const dbConfig = config.get('database');
+
+    mongoose
+        .connect(dbConfig, {
+            useCreateIndex: true,
+            useNewUrlParser: true
+        })
+        .then(() => winston.info(`Connected to ${dbConfig} ...`))
+        .catch((err) => {
+            // eslint-disable-next-line no-console
+            console.log({
+                err
+            });
+        });
+};
